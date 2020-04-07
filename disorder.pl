e3d981a3a8f20eb71013b234b739d38aa1b232b7
Gamiraude <gamiraude@gmail.com>
2020-04-07 13:02:45 +0200

    add initial pl files

---
+++ disorder.pl
@@ -0,0 +1,14 @@
+disorder(anxiety,[worrying, fast_heart_rate, shaking,sweating]).
+disorder(anorexia,[low_weight, fear_of_gaining_weight, strong_desire_to_be_thin, food_restrictions]).
+disorder(autism,[trouble_with_social_interaction, impaired_communication, restricted_interests, repetitive_behavior]).
+disorder(bipolar_disorder,[depression, mood_lability]).
+disorder(borderline_personality_disorder, [unstable_relationships, impulsivity, suicidal_behavior, selfharm, feeling_of_emptiness, anger, detached_from_reality]).
+disorder(down_syndrome,[delayed_physical_growth,intellectual_disability,characteristic_facial_features]).
+disorder(delirium,[inattention,memory_impairment,disorientation,mood_lability, sleep_changes]).
+disorder(dementia,[memory_impairment,decresead_motivation]).
+disorder(obsessive_compulsive_disorder,[check_things_repeatedly,perform_routines_repeatedly]).
+disorder(panic_disorder,[fear,palpitations,sweating,shaking,numbness]).
+disorder(parkinsons_disease,[shaking,rigidity,slowness_of_movement,difficulty_walking]).
+disorder(schizophrenia,[hallucinations, delusions, confused_thinking]).
+disorder(depression,[sadness,difficulty_in_thinking,difficulty_in_concentration,increase_in_appetite,decrease_in_appetite,sleep_changes]).
+disorder(obesity,[increased_fat]).
